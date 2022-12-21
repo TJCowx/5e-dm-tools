db7e39e1105f1d83b2ba4c3b4ef88c6a993ea2f4
Travis <tcowx21@gmail.com>
2022-12-20 21:40:35 -0500

    Added nextron

---
+++ renderer/pages/home.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+
+function Home() {
+  return (
+    <React.Fragment>
+      <Head children={<title>Home - Nextron (with-typescript)</title>} />
+      <div>
+        <p>
+          ⚡ Electron + Next.js ⚡ -
+          <Link href="/next">
+            <a>Go to next page</a>
+          </Link>
+        </p>
+        <img src="/images/logo.png" />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Home;
