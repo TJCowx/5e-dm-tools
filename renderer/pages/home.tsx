ff81267bbc7f33abc2d407c3e5f706c9d58f9760
Travis <tcowx21@gmail.com>
2023-01-16 19:15:14 -0500

    Updated home page to link to different pages

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,20 +1,38 @@
-import Button from '@mui/material/Button';
+import { List, ListItemButton } from '@mui/material';
 import Layout from 'components/Layout/Layout';
+import ListItemText from 'components/List/ListItemText';
+import NextLink from 'next/link';
 import { FC } from 'react';
-import { logMessage } from 'utils/logUtils';
 
 const Home: FC = () => {
-  const testLog = () => {
-    try {
-      throw new Error('This is an error');
-    } catch (e) {
-      logMessage('error', 'This is a test message');
-    }
-  };
-
   return (
     <Layout title="Home">
-      <Button onClick={testLog}>Test Log</Button>
+      <List>
+        <NextLink href="/initiative" passHref>
+          <ListItemButton component="a">
+            <ListItemText
+              primary="Initiative Tracker"
+              secondary="Start tracking initiative for an encounter."
+            />
+          </ListItemButton>
+        </NextLink>
+        <NextLink href="/creatures" passHref>
+          <ListItemButton component="a">
+            <ListItemText
+              primary="Creatures List"
+              secondary="View, create, and edit creatures."
+            />
+          </ListItemButton>
+        </NextLink>
+        <NextLink href="/connection/connect" passHref>
+          <ListItemButton component="a">
+            <ListItemText
+              primary="Change Connection"
+              secondary="Go back and change your connection string."
+            />
+          </ListItemButton>
+        </NextLink>
+      </List>
     </Layout>
   );
 };
