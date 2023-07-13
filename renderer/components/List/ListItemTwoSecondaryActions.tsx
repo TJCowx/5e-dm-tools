8f26b92c1bf422020085560a6e71e7e760124b6f
Travis <tcowx21@gmail.com>
2023-07-13 19:42:44 -0400

    CLEANING THE SLATE

--- renderer/components/List/ListItemTwoSecondaryActions.tsx
+++
@@ -1,9 +0,0 @@
-import { ListItem } from '@mui/material';
-import { styled } from '@mui/system';
-
-export default styled(ListItem)(() => ({
-  paddingRight: '96px',
-  '& .MuiListItemSecondaryAction-root > *:first-of-type': {
-    marginRight: '4px',
-  },
-}));
--- renderer/utils/rollUtils.ts
+++
@@ -1,5 +0,0 @@
-const randomInteger = (min: number, max: number) =>
-  Math.floor(Math.random() * (max - min + 1)) + 1;
-
-/* eslint-disable import/prefer-default-export */
-export const rollD20 = (modifier = 0) => randomInteger(1, 20) + modifier;
