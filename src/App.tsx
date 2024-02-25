c51c3c2776e5489f83bb34cccfe618edec282ea3
TJCowx <tcowx21@gmail.com>
2024-02-25 15:38:48 -0500

    Updated configs

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
