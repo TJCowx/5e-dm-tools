80e29039bf80265d8f0fbdbfd9a848dcca13b94e
TJCowx <tcowx21@gmail.com>
2024-02-29 21:45:58 -0500

    Added endSlotComponent to PageHeader

--- src/components/Layout/PageHeader.tsx
+++ src/components/Layout/PageHeader.tsx
@@ -1,15 +1,31 @@
 import { Divider, Typography } from '@mui/material';
 import { styled } from '@mui/system';
+import { ReactNode } from 'react';
+
+const TitleContainer = styled('div')(() => ({
+  display: 'flex',
+  '& .end-slot': { marginLeft: 'auto' },
+}));
 
 const HeaderDivider = styled(Divider)(() => ({
   marginTop: '8px',
   marginBottom: '16px',
 }));
 
-export default function PageHeader({ title }: { title: string }) {
+interface Props {
+  title: string;
+  endSlotComponent?: ReactNode;
+}
+
+export default function PageHeader({ title, endSlotComponent }: Props) {
   return (
     <>
-      <Typography variant="h5">{title}</Typography>
+      <TitleContainer>
+        <Typography variant="h5">{title}</Typography>
+        {!!endSlotComponent && (
+          <div className="end-slot">{endSlotComponent}</div>
+        )}
+      </TitleContainer>
       <HeaderDivider />
     </>
   );
--- src/pages/sources/index.tsx
+++ src/pages/sources/index.tsx
@@ -1,10 +1,32 @@
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconButton, Tooltip } from '@mui/material';
+import { useState } from 'react';
+
 import { PageHeader } from '@components/Layout';
 
 export default function SourcesPage() {
+  const [isAddOpen, setIsAddOpen] = useState(false);
+
+  // TODO: Get list of sources
   return (
     <>
-      <PageHeader title="Sources" />
+      <PageHeader
+        title="Sources"
+        endSlotComponent={
+          <Tooltip title="Add new source">
+            <IconButton
+              className="add-source-btn"
+              aria-label="Add new source"
+              onClick={() => setIsAddOpen(true)}
+            >
+              <FontAwesomeIcon icon={faPlus} />
+            </IconButton>
+          </Tooltip>
+        }
+      />
       <div>This is where stuff goes</div>
+      {isAddOpen && <div>TODO:</div>}
     </>
   );
 }
